f2ada1b7907fa041cd43aa76d1fac12ebcf36675
sushmapuli23 <sushmapuli23@gmail.com>
2020-10-28 13:22:21 +0530

    final commit

---
+++ main.js
@@ -0,0 +1,19 @@
+let addtodoButton = document.getElementById('addtodo');
+let todoContainer = document.getElementById('todoContainer');
+let inputField = document.getElementById('inputField');
+
+addtodoButton.addEventListener('click',function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    todoContainer.appendChild(paragraph);
+    inputField.value = '';
+    paragraph.addEventListener('click',function(){
+        paragraph.style.textDecoration = 'line-through';
+    })
+
+    paragraph.addEventListener('dblclick',function(){
+        todoContainer.removeChild(paragraph);
+    })
+
+});
